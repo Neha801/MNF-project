ac47e88796bad8db05258c6ab0e05ab7d1672f0a
Neha Kumari <55044120+Neha801@users.noreply.github.com>
2021-02-15 21:23:07 +0530

    Update urls.py

--- mnfapp/urls.py
+++ mnfapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('mynarration', views.mynarration, name="mynarration"),
     path('empty', views.empty, name="empty"),
     path('newempty', views.newempty, name="newempty"),
-
+    path('viewmore', views.viewmore, name="viewmore"),
 
 
 ]
