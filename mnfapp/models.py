03fd06988cd8701fb415ffa9b91b6c075887ee2f
Neha Kumari <55044120+Neha801@users.noreply.github.com>
2021-02-26 13:19:02 +0530

    bbp

--- mnfapp/models.py
+++ mnfapp/models.py
@@ -3,12 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-
-
 class Scripts(models.Model):
     script_title = models.CharField(max_length=100, null=True)
     author_name = models.CharField(max_length=100, null=True)
@@ -33,3 +27,38 @@ class Uploads(models.Model):
 
     def __str__(self):
         return str(self.uploaded_script)
+
+class Uploadvideo(models.Model):
+    name= models.CharField(max_length=500,blank=True)
+    recfile= models.FileField(upload_to='diybbp/')
+
+    def __str__(self):
+        return self.name + ": " + str(self.recfile)
+
+class Uploadrefer(models.Model):
+    refername= models.CharField(max_length=500,blank=True)
+    refervideo= models.FileField(upload_to='diybbp/')
+
+    def __str__(self):
+        return self.refername + ": " + str(self.refervideo)
+
+class Uploaddo(models.Model):
+    doname= models.CharField(max_length=500,blank=True)
+    dovideo= models.FileField(upload_to='diybbp/')
+
+    def __str__(self):
+        return self.doname + ": " + str(self.dovideo)
+
+class Uploadlive(models.Model):
+    livename= models.CharField(max_length=500,blank=True)
+    livevideo= models.FileField(upload_to='diybbp/')
+
+    def __str__(self):
+        return self.livename + ": " + str(self.livevideo)
+
+class Uploadboundary(models.Model):
+    boundaryname= models.CharField(max_length=500,blank=True)
+    boundaryvideo= models.FileField(upload_to='diybbp/')
+
+    def __str__(self):
+        return self.boundaryname+ ": " + str(self.boundaryvideo)
